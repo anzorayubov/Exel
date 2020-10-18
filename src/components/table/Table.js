8f67fe47dd174bf07834a1f6b0edc2a118ebbf29
Anzor <48889137+anzorayubov@users.noreply.github.com>
2020-10-18 19:21:05 +0300

    test events

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,25 @@ import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExelComponent {
     static className = 'exel__table'
+
+    constructor($root) {
+        super($root, {
+            listeners: ['click', 'mousedown', 'mousemove'],
+        });
+    }
+
     toHTML() {
         return createTable()
+    }
+
+    onClick() {
+        console.log('click')
+    }
+
+    onMouseDown() {
+        console.log('mouseDown')
+    }
+    onMouseMove() {
+        console.log('mousemove')
     }
 }
