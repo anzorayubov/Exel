d007e6420cbd12511828d67025a472371c1ddb6d
Anzor <48889137+anzorayubov@users.noreply.github.com>
2020-11-06 22:48:03 +0300

    add resize for for columns

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import {$} from "@core/dom";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -15,9 +16,20 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event) {
-      if (event.target.dataset.resize) {
-        
-      }
+        if (event.target.dataset.resize) {
+            const $resizer = $(event.target)
+            const $parent = $resizer.closest('[data-type="resizable"]')
+            const cords = $parent.getCords()
+
+            document.onmousemove = e => {
+                const delta = e.pageX - cords.right
+                $parent.$el.style.width = (cords.width + delta) + 'px'
+            }
+
+            document.onmouseup = () => {
+                document.onmousemove = null
+            }
+        }
     }
 
 }
