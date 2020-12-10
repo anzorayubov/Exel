e06ecd7c789ab83a292dfd01fda0bd7a7d691f66
Anzor <48889137+anzorayubov@users.noreply.github.com>
2020-12-10 15:21:39 +0300

    add CSS method for set styles. like jq :)

--- src/core/dom.js
+++ src/core/dom.js
@@ -58,6 +58,10 @@ class Dom {
         return this.$el.querySelectorAll(selector)
     }
 
+    css(styles = {}) {
+        Object.keys(styles).forEach(key => this.$el.style[key] = styles[key])
+    }
+
 }
 
 // event.target
