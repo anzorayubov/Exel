7d632600fe1a39344200c0caee03b068a862689b
Anzor <48889137+anzorayubov@users.noreply.github.com>
2021-01-15 13:29:30 +0300

    render table state from storage

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,31 +1,38 @@
 const CODES = {
-  A: 65,
-  Z: 90
+    A: 65,
+    Z: 90
 }
 
-// function toCell(row, col) {
-//   return `
-//     <div class="cell" contenteditable data-col="${col}"></div>
-//   `
-// }
+const DEFAULT_WIDTH = 120
 
-function toCell(row) {
-  return function(_, col) {
-    return `
+function getWidth(state, index) {
+    return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
+function toCell(state, row) {
+    return function (_, col) {
+        const width = getWidth(state.colState, col)
+        return `
       <div 
         class="cell" 
         contenteditable 
         data-col="${col}"
         data-type="cell"
         data-id="${row}:${col}"
+        style="width: ${width}"
       ></div>
     `
-  }
+    }
 }
 
-function toColumn(col, index) {
-  return `
-    <div class="column" data-type="resizable" data-col="${index}">
+function toColumn({col, index, width}) {
+    return `
+    <div 
+      class="column" 
+      data-type="resizable" 
+      data-col="${index}" 
+      style="width: ${width}"
+    >
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
@@ -33,8 +40,8 @@ function toColumn(col, index) {
 }
 
 function createRow(index, content) {
-  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
-  return `
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+    return `
     <div class="row" data-type="resizable">
       <div class="row-info">
         ${index ? index : ''}
@@ -46,30 +53,38 @@ function createRow(index, content) {
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODES.A + index)
+    return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
-  const rows = []
+function withWidthFrom(state) {
+    return function (col, index) {
+        return {
+            col, index, width: getWidth(state.colState, index)
+        }
+    }
+}
 
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('')
+export function createTable(rowsCount = 15, state = {}) {
+    const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
+    const rows = []
 
-  rows.push(createRow(null, cols))
-
-  for (let row = 0; row < rowsCount; row++) {
-    const cells = new Array(colsCount)
+    const cols = new Array(colsCount)
         .fill('')
-        // .map((_, col) => toCell(row, col))
-        .map(toCell(row))
+        .map(toChar)
+        .map(withWidthFrom(state))
+        .map(toColumn)
         .join('')
 
-    rows.push(createRow(row + 1, cells))
-  }
+    rows.push(createRow(null, cols))
 
-  return rows.join('')
+    for (let row = 0; row < rowsCount; row++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell(state, row))
+            .join('')
+
+        rows.push(createRow(row + 1, cells))
+    }
+
+    return rows.join('')
 }
