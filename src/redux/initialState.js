b99acbd991759dba9620bcb85825e0f3d2322580
Anzor <48889137+anzorayubov@users.noreply.github.com>
2021-01-17 15:58:35 +0300

    save data in cells

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -2,7 +2,10 @@ import {storage} from "@core/utils";
 
 const defaultState = {
     rowState: {},
-    colState: {}
+    colState: {},
+    dataState: {},
+    currentText: '',
+
 }
 
 export const initialState = storage('exel-state') ? storage('exel-state') : defaultState
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,14 +1,23 @@
 // Pure function
-import {TABLE_RESIZE} from "@/redux/types";
+import {CHANGE_TEXT, TABLE_RESIZE} from "@/redux/types";
 
 export function rootReducer(state, action) {
     let field
+    let prevState
     switch (action.type) {
         case TABLE_RESIZE:
             field = action.data.type === 'col' ? 'colState' : 'rowState'
-            const prevState = state[field] || {}
+            prevState = state[field] || {}
             prevState[action.data.id] = action.data.value
             return {...state, [field]: prevState}
+        case CHANGE_TEXT:
+            prevState = state['dataState'] || {}
+            prevState[action.data.id] = action.data.value
+            return {
+                ...state,
+                currentText: action.data.value,
+                dataState: prevState
+            }
         default:
             return state
     }
