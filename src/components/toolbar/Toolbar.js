8328a781d740f435bfb8cc976b5ae435204d256b
Anzor <48889137+anzorayubov@users.noreply.github.com>
2021-01-24 15:27:25 +0300

    styles of cells

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,43 +1,39 @@
 import {createToolbar} from '@/components/toolbar/toolbar.template'
 import {$} from '@core/dom'
 import {ExcelStateComponent} from '@core/ExcelStateComponent'
+import {defaultStyles} from '@/constants'
 
 export class Toolbar extends ExcelStateComponent {
-  static className = 'excel__toolbar'
+    static className = 'excel__toolbar'
 
-  constructor($root, options) {
-    super($root, {
-      name: 'Toolbar',
-      listeners: ['click'],
-      ...options
-    })
-  }
-
-  prepare() {
-    const initialState = {
-      textAlign: 'left',
-      fontWeight: 'normal',
-      textDecoration: 'none',
-      fontStyle: 'normal'
+    constructor($root, options) {
+        super($root, {
+            name: 'Toolbar',
+            listeners: ['click'],
+            ...options
+        })
     }
-    this.initState(initialState)
-  }
 
-  get template() {
-    return createToolbar(this.state)
-  }
-
-  toHTML() {
-    return this.template
-  }
-
-  onClick(event) {
-    const $target = $(event.target)
-    if ($target.data.type === 'button') {
-      const value = JSON.parse($target.data.value)
-      const key = Object.keys(value)[0]
-      this.setState({[key]: value[key]})
-      console.log(this.state)
+    prepare() {
+        this.initState(defaultStyles)
     }
-  }
+
+    get template() {
+        return createToolbar(this.state)
+    }
+
+    toHTML() {
+        return this.template
+    }
+
+    onClick(event) {
+        const $target = $(event.target)
+        if ($target.data.type === 'button') {
+            const value = JSON.parse($target.data.value)
+            this.$emit('toolbar:applyStyle', value)
+
+            const key = Object.keys(value)[0]
+            this.setState({[key]: value[key]})
+        }
+    }
 }
--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -1,9 +1,9 @@
 function toButton(button) {
-  const meta = `
+    const meta = `
     data-type="button"
     data-value='${JSON.stringify(button.value)}'
   `
-  return `
+    return `
     <div 
       class="button ${button.active ? 'active' : ''}"
       ${meta}
@@ -16,38 +16,42 @@ function toButton(button) {
   `
 }
 
-export function createToolbar(state) {
-  const buttons = [
-    {
-      icon: 'format_align_left',
-      active: false,
-      value: {textAlign: 'left'}
-    },
-    {
-      icon: 'format_align_center',
-      active: false,
-      value: {textAlign: 'center'}
-    },
-    {
-      icon: 'format_align_right',
-      active: false,
-      value: {textAlign: 'right'}
-    },
-    {
-      icon: 'format_bold',
-      active: state['fontWeight'] === 'bold',
-      value: {fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold'}
-    },
-    {
-      icon: 'format_italic',
-      active: false,
-      value: {fontStyle: 'italic'}
-    },
-    {
-      icon: 'format_underlined',
-      active: false,
-      value: {textDecoration: 'underline'}
-    }
-  ]
-  return buttons.map(toButton).join('')
+export function createToolbar(s) {
+    const buttons = [
+        {
+            value: {textAlign: 'left'},
+            icon: 'format_align_left',
+            active: s['textAlign'] === 'left'
+        },
+        {
+            value: {textAlign: 'center'},
+            icon: 'format_align_justify',
+            active: s['textAlign'] === 'center'
+        },
+        {
+            value: {textAlign: 'right'},
+            icon: 'format_align_right',
+            active: s['textAlign'] === 'right'
+        },
+        {
+            value: {fontWeight: s['fontWeight'] === 'bold' ? 'normal' : 'bold'},
+            icon: 'format_bold',
+            active: s['fontWeight'] === 'bold'
+        },
+        {
+            value: {
+                textDecoration: s['textDecoration'] === 'underline'
+                    ? 'none'
+                    : 'underline'
+            },
+            icon: 'format_underlined',
+            active: s['textDecoration'] === 'underline'
+        },
+        {
+            value: {fontStyle: s['fontStyle'] === 'italic' ? 'normal' : 'italic'},
+            icon: 'format_italic',
+            active: s['fontStyle'] === 'italic'
+        }
+    ]
+    return buttons.map(toButton).join('')
 }
